dc69e469bd8782f6861cdb97be387c6762cb7c34
Nuno Peixoto <nuno_peixoto1997@hotmail.com>
2024-03-19 23:44:01 +0000

    Upgrade angular to 16

--- src/app/auth/no-auth-guard.ts
+++ src/app/auth/no-auth-guard.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class NoAuthGuard implements CanActivate {
+export class NoAuthGuard  {
 
   constructor(private authService: AuthService, private router: Router) {}
 
